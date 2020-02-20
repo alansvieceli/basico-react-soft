64fe22de120b6ed9026aff8f57c332f7d70d4436
Alan Vieceli <alan.vieceli@softplan.com.br>
2020-02-20 18:23:46 -0300

    useEffect

--- src/componentes/Logo.js
+++ src/componentes/Logo.js
@@ -1,12 +1,29 @@
-import React, {useState, Fragment} from 'react';
+import React, {useState, Fragment, useEffect} from 'react';
 import logo from '../logo.svg';
 import './Logo.css';
 
 const Logo = (props) => {
 
-    const [repeat, setRepeat] = useState(1)
+    const [repeat, setRepeat] = useState();
+    const [countUP, setCountUp] = useState();
+    const [countDow, setCountDown] = useState()
+
+    useEffect(() => {
+      setRepeat(1);
+      setCountUp(0);
+      setCountDown(0);
+    }, []);
+
+    useEffect(() => {
+      console.log('repeat updated');
+    }, [repeat]);
   	
     const clickHandler = (v) => {
+        if (v > 0)
+          setCountUp((x) => x + 1)
+        else
+          setCountDown((x) => x + 1);
+
         setRepeat((x) => x + v);
     }
 
@@ -20,8 +37,8 @@ const Logo = (props) => {
         {elements}
         </div>
         <div className="App-logo-buttons">
-          <button disabled={repeat === 6} onClick={() => clickHandler(1)}> Logo + </button>
-          <button disabled={repeat === 1} onClick={() => clickHandler(-1)}> Logo - </button>
+          <button disabled={repeat === 6} onClick={() => clickHandler(1)}> Logo + ({countUP})</button>
+          <button disabled={repeat === 1} onClick={() => clickHandler(-1)}> Logo - ({countDow})</button>
         </div>
       </Fragment>
     );
